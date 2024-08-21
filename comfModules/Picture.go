79c459355db5f88097a2e817c32259973b0fa8e9
Sindi_Hall <146654192+Sindihall@users.noreply.github.com>
2024-08-21 22:44:23 +0300

    Update Picture.go

--- comfModules/Picture.go
+++ comfModules/Picture.go
@@ -1,1 +1,31 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+type Image struct {
+	width, height int
+}
+
+func (im Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, im.width, im.height)
+}
+
+func (im Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (im Image) At(x, y int) color.Color {
+	v := uint8((y * x) % 255)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{width: 256, height: 256}
+	pic.ShowImage(m)
+}
 
